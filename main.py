47b35f872920201fd13a389ac734591ba82a630c
krook98 <sokalski.jakub98@gmail.com>
2022-03-07 01:40:14 +0100

    Navigation bar working, every post opens as separate page

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+posts = requests.get("https://api.npoint.io/1581d18ce8f2adc6be48").json()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', all_posts=posts)
+
+
+@app.route('/about')
+def about_me():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
